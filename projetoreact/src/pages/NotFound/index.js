33acc5aefb5553c15f9028d7aa1c3bfbe0bb5bd1
Evandro Mendes <evandromende@gmail.com>
2024-06-11 01:42:23 -0300

    Fix: NotFound

--- projetoreact/src/pages/NotFound/index.js
+++ projetoreact/src/pages/NotFound/index.js
@@ -6,7 +6,7 @@ const NotFound = () => {
             <h3 className='text-center text-3xl md:tex-4x1 lg:text-5xl font-semibold text-gray-900'>404 - Not Found</h3>
 
             <div className='bg-primary px-6 text-white text-center rounded-full transition duration-700 hover:scale-105'>
-            <Link to='/'>Voltar para Página Principal Home</Link>
+            <Link to='/'>Voltar para Página Principal</Link>
             </div>
             
         </div>
