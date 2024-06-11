9b26133eee9232acddb7a2bfd55754cac8409854
Evandro Mendes <evandromende@gmail.com>
2024-06-11 00:31:50 -0300

    Feat: Atividade01

---
+++ reactatividade2/src/pages/NotFound/index.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return (
+        <div>
+            <h3 className='text-center text-3xl md:tex-4x1 lg:text-5xl font-semibold text-gray-900'>404 - Not Found</h3>
+
+            <div className='bg-primary px-6 text-white text-center rounded-full transition duration-700 hover:scale-105'>
+            <Link to='/'>Voltar para Página Principal</Link>
+            </div>
+            
+        </div>
+    )
+}
+
+export default NotFound;
